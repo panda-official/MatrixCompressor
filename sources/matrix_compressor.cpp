b0b1cb37c2c612fa9e52f0bc3d5202c50d2c0d4b
Kataev Victor <kataev.victor.1234@gmail.com>
2022-12-29 11:08:53 +0000

    Add fpzip

--- sources/matrix_compressor.cpp
+++ sources/matrix_compressor.cpp
@@ -1,6 +1,9 @@
 #include "matrix_compressor/matrix_compressor.h"
 
+#include <fpzip.h>
 #include <streamvbyte.h>
+
+#include <iostream>
 
 namespace matrix_compressor {
 
@@ -20,6 +23,26 @@ void compress(const blaze::CompressedVector<float>& vector) {
   compressed.resize(streamvbyte_max_compressedbytes(indexes.size()));
   size_t compressed_size =
       streamvbyte_encode(indexes.data(), indexes.size(), compressed.data());
+
+  std::cout << "Compressed " << indexes.size() << " integers down to "
+            << compressed_size << " bytes." << std::endl;
+
+  size_t N = values.size();
+  std::vector<uint8_t> compressed_values(sizeof(float) * N + 1024);
+  FPZ* fpz =
+      fpzip_write_to_buffer(reinterpret_cast<void*>(compressed_values.data()),
+                            sizeof(float) * N + 1024);
+  fpz->type = FPZIP_TYPE_FLOAT;
+  fpz->prec = 0;
+  fpz->nx = N;
+  fpz->ny = 1;
+  fpz->nz = 1;
+  fpz->nf = 1;
+  size_t hs = fpzip_write_header(fpz);
+  size_t ds = fpzip_write(fpz, static_cast<void*>(values.data()));
+  fpzip_write_close(fpz);
+  std::cout << "Compressed " << N << " floats down to " << hs + ds << " bytes."
+            << std::endl;
 }
 
 }  // namespace matrix_compressor
